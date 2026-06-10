349359ed586e96efaf9f69be2361c3132106c08d
s-quare <okeyprosperebubealex14@gmail.com>
2026-06-10 20:00:42 +0100

    optimize commot

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   allowedDevOrigins: ['10.175.239.32'],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
